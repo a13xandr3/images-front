8e56fa2db2228d7d2f763f957def6a1f8746232b
a13xandr3 <estevesalves@icloud.com>
2026-09-12 00:59:58 -0300

    feat: add application layout shell

---
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '../footer/footer';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [Header, RouterOutlet, Footer],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+})
+export class MainLayout {}
